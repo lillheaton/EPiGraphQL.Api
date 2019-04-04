15eea9efafe336ae280c63b5a773cca0cb852510
Emil Olsson <emilolsson93@gmail.com>
2019-04-04 09:06:14 +0200

    Framework build version issue

--- tools/tasks/relocate.js
+++ tools/tasks/relocate.js
@@ -4,8 +4,8 @@ export default function relocate(settings) {
   const { projectName, sourcePath, tempPath } = settings
   const dllName = `${projectName}.dll`
 
-  mkdir('-p', `${tempPath}/lib/net471`)
-  cp(`${sourcePath}/bin/${dllName}`, `${tempPath}/lib/net471/${dllName}`)
+  mkdir('-p', `${tempPath}/lib/net461`)
+  cp(`${sourcePath}/bin/${dllName}`, `${tempPath}/lib/net461/${dllName}`)
   cp(`${sourcePath}/${projectName}.nuspec`, `${tempPath}/${projectName}.nuspec`)
 
   return Promise.resolve()
